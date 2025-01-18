d9db7e419bf8ce6558a6f35552f67e08344d6821
Murali Dharan S <89293722+murali2601@users.noreply.github.com>
2025-01-18 21:36:29 +0530

    included html,css,js

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('clickMeButton').addEventListener('click', function () {
+    const message = document.getElementById('message');
+    message.textContent = 'Hello, GitHub is awesome!';
+    message.style.color = '#4caf50';
+  });
+  
